fcb83462c5604162f890a7e15be5b7ebfac0e9ba
Bethany <morphforlife@gmail.com>
2022-07-17 21:41:55 -0500

    Part 1 fin

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
-
+  photosTitle = 'Favorite Pictures';
+  image1 = "https://i.imgur.com/WqMD23g.jpg";
+  image2 = "https://i.imgur.com/nULJ1N9.jpg";
+  image3 = "https://i.imgur.com/xtXDbn7.jpg";
+  image4 = "https://i.imgur.com/puILpgi.jpg";
   constructor() { }
 
   ngOnInit() {
