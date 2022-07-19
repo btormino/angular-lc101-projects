f671bd9af03365d92a3bf0ff74764fd7114a6309
Bethany <morphforlife@gmail.com>
2022-07-19 09:34:37 -0500

    added send on mission button and began addToCrew function

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -23,8 +23,9 @@ export class CandidatesComponent implements OnInit {
   ngOnInit() {
   }
 
-  // Code the addToCrew function here:
+   addToCrew(person: object){
 
+  }
 
   // BONUS: Code the changeMissionName function here:
 
