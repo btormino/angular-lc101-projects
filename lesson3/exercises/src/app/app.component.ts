440ae4d46746d53f901dd5b3568dd0ce5460df11
Bethany <morphforlife@gmail.com>
2022-07-22 22:48:21 -0500

    Everything up to New Requirements + I tried to add rocketImage to handleTakeOff

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,64 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+    let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+    rocketImage.style.left = movement;
+    this.width - this.width + 10000;     
+    } 
+    else if (direction === 'left') {
+    let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+    rocketImage.style.left = movement;
+    this.width - this.width - 10000;    
+    }
+    else if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.width - this.width - 10000;    
+      }
+      else {
+        let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+        rocketImage.style.bottom = movement;
+        this.width - this.width + 10000;    
+        }
+  }
+
+  handleTakeOff(rocketImage) {
+    let result = window.confirm('Are you sure the shuttle is ready for take off?');
+    if (result) {
+        this.color = 'blue';
+        this.height = 10000;
+        this.width = 0;
+        this.message = 'Shuttle in flight.';
+        rocketImage.style.bottom = '10000 px';
+    }
+  }
+    
+
+    handleLand(rocketImage) {
+      let result = window.confirm('The shuttle is landing. Landing gear engaged.');
+      if (result) {
+        this.color = 'green';
+        this.height = 0;
+        this.width = 0;
+        this.message = 'The shuttle has landed.';
+        rocketImage.style.bottom = '0 px';
+      }
+    }
+
+    handleAbortMission(rocketImage) {
+      let result = window.confirm('Are you sure you want to abort the mission?');
+      if (result) {
+        this.color = 'red';
+        this.height = 0;
+        this.width = 0;
+        this.message = 'Mission aborted.';
+        rocketImage.style.bottom = '0 px';
+      }
+    }
+
+   
+  
 }
